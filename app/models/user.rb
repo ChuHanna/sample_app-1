48716e437c0b0cb5c0c707c433f8337240285593
Le Anh Thanh <le.anh.thanh@sun-asterisk.com>
2021-07-18 18:24:18 +0700

    Chapter 9: Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save :downcase_email
 
   validates :name, presence: true,
@@ -12,15 +13,30 @@ class User < ApplicationRecord
   has_secure_password
 
   class << self
-    # Returns the hash digest of the given string.
     def digest string
-      cost = if ActiveModel::SecurePassword.min_cost
-               BCrypt::Engine::MIN_COST
-             else
-               BCrypt::Engine.cost
-             end
+      min_cost = ActiveModel::SecurePassword.min_cost
+      cost = min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
   end
 
   private
